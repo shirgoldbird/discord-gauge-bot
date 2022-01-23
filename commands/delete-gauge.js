ba2929e3e70d31ef0cb843d4cfa5fdb193466f4d
Shir Goldberg <me@shirgoldbird.com>
2022-01-23 13:18:34 -0800

    delete gauge from list of all gauges when removing

--- commands/delete-gauge.js
+++ commands/delete-gauge.js
@@ -1,31 +1,49 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { gaugeSelectList } = require('../helpers/select-gauge');
 const Keyv = require('keyv')
 const KeyvFile = require('keyv-file').KeyvFile
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('delete')
-		.setDescription('Delete a gauge')
-        .addStringOption(option => option.setName('gauge-name').setDescription('Enter the gauge\'s name').setRequired(true)),
+		.setDescription('Delete a gauge'),
 	async execute(interaction) {
-        await interaction.deferReply();
-        const gaugeName = interaction.options.getString('gauge-name');
-        const keyv = new Keyv({
-            store: new KeyvFile({
-                filename: `data/gauges.json`, // the file path to store the data
-            })
-        });
-        const gauge = await keyv.get(gaugeName);
-        if (gauge) {
-            console.log(`Deleting gauge ${gaugeName}`)
-            const gaugeGoal = gauge.goal;
-            const gaugeValue = gauge.value;
-            console.log(`${gaugeName} status: ${gaugeValue} / ${gaugeGoal}`);
-            await keyv.delete(gaugeName);
-		    await interaction.editReply(`Deleted gauge ${gaugeName} (${gaugeValue} / ${gaugeGoal})`);
-        } else {
-            console.log(`No gauge named ${gaugeName} to delete!`)
-            await interaction.editReply(`No gauge named ${gaugeName} to delete!`);
+        if (interaction.isCommand()) {
+            console.log('Got delete command, returning select menu')
+            // Get a select list of all gauges
+            await gaugeSelectList(interaction);
+        } else if (interaction.isSelectMenu()) {
+            await interaction.deferUpdate();
+            console.log(`Attempting to get value for ${interaction.values[0]}`)
+            const gaugeName = interaction.values[0];
+            const keyv = new Keyv({
+                store: new KeyvFile({
+                    filename: `data/gauges.json`, // the file path to store the data
+                })
+            });
+            const gauge = await keyv.get(gaugeName);
+            if (gauge) {
+                console.log(`Deleting gauge ${gaugeName}`)
+                const gaugeGoal = gauge.goal;
+                const gaugeValue = gauge.value;
+                console.log(`${gaugeName} status: ${gaugeValue} / ${gaugeGoal}`);
+                await keyv.delete(gaugeName);
+                let allGauges = await keyv.get('all');
+                if (allGauges) {
+                    const index = allGauges.indexOf(gaugeName);
+                    if (index > -1) {
+                        allGauges.splice(index, 1);
+                    }
+                } else {
+                    allGauges = [];
+                }
+                await keyv.set('all', allGauges);
+
+                await interaction.editReply({ content: `Deleted gauge ${gaugeName} (${gaugeValue} / ${gaugeGoal})`, components: [] });
+            } else {
+                console.log(`No gauge named ${gaugeName} to delete!`)
+                await interaction.editReply({ content: `No gauge named ${gaugeName} to delete!`, components: [] });
+            }
         }
 	},
 };

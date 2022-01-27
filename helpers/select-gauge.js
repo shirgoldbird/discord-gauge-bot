737312be15292a012b20e15b7c737855b7c2de39
Shir Goldberg <me@shirgoldbird.com>
2022-01-26 19:31:51 -0800

    add safety check

--- helpers/select-gauge.js
+++ helpers/select-gauge.js
@@ -13,29 +13,34 @@ module.exports = {
         });
         // get all gauges
         let allGauges = await keyv.get('all');
-        console.log(`Got ${allGauges.length} gauges`)
-        let allGaugeOptions = [];
-        // turn them into options for select list
-        allGauges.forEach((gauge) => {
-            let gaugeOption = {
-                label: gauge,
-                value: gauge
+        if (allGauges) {
+            console.log(`Got ${allGauges.length} gauges`)
+            let allGaugeOptions = [];
+            // turn them into options for select list
+            allGauges.forEach((gauge) => {
+                let gaugeOption = {
+                    label: gauge,
+                    value: gauge
+                }
+                allGaugeOptions.push(gaugeOption)
+            });
+
+            if (allGaugeOptions.length == 0) {
+                await interaction.editReply({ content: 'No gauges available. Add one with /create.' });
+            } else {
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageSelectMenu()
+                            .setCustomId('select')
+                            .setPlaceholder('Nothing selected')
+                            .addOptions(allGaugeOptions),
+                );
+
+                await interaction.editReply({ content: 'Select a gauge:', components: [row], ephemeral: true });
             }
-            allGaugeOptions.push(gaugeOption)
-        });
-
-        if (allGaugeOptions.length == 0) {
-            await interaction.editReply({ content: 'No gauges available. Add one with /create.' });
         } else {
-            const row = new MessageActionRow()
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId('select')
-                        .setPlaceholder('Nothing selected')
-                        .addOptions(allGaugeOptions),
-            );
-
-            await interaction.editReply({ content: 'Select a gauge:', components: [row], ephemeral: true });
+            console.log("No gauges to get!");
+            await interaction.editReply({ content: 'No gauges available. Add one with /create.' });
         }
     },
 };
